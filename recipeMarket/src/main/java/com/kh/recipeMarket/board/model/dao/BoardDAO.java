bacd36feb6d94bcf89a1a778059b2b265f4900fc
kimdonghyeon <kdh9086@naver.com>
2020-04-08 11:00:22 +0900

    자유게시판 수정

---
+++ recipeMarket/src/main/java/com/kh/recipeMarket/board/model/dao/BoardDAO.java
@@ -0,0 +1,28 @@
+package com.kh.recipeMarket.board.model.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.recipeMarket.board.model.vo.Board;
+import com.kh.recipeMarket.board.model.vo.PageInfo;
+
+@Repository("bDAO")
+public class BoardDAO {
+
+	public int getListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("boardMapper.getListCount");
+	}
+
+	public ArrayList<Board> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
+		int offset =(pi.getCurrentPage()-1) * pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		
+		return (ArrayList)sqlSession.selectList("boardMapper.selectList", null, rowBounds);
+	}
+	
+	
+	
+}
---
+++ recipeMarket/src/main/java/com/kh/recipeMarket/board/model/exception/BoardException.java
@@ -0,0 +1,10 @@
+package com.kh.recipeMarket.board.model.exception;
+
+public class BoardException extends RuntimeException{
+	public BoardException() {}
+	
+	public BoardException(String msg) {
+		super(msg);
+	}
+
+}
