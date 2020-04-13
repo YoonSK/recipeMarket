5a644e4c2b353990efa895b58d2a5c6eb3a78447
kimdonghyeon <kdh9086@naver.com>
2020-04-13 14:07:07 +0900

    자유게시판

--- recipeMarket/src/main/java/com/kh/recipeMarket/board/model/vo/Board.java
+++ recipeMarket/src/main/java/com/kh/recipeMarket/board/model/vo/Board.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 public class Board {
 	private int postNo;
 	private int memberNo;
+	private String nickName;
 	private String content;
 	private Date createDate;
 	private Date modifyDate;
@@ -19,6 +20,21 @@ public class Board {
 		super();
 		this.postNo = postNo;
 		this.memberNo = memberNo;
+		this.content = content;
+		this.createDate = createDate;
+		this.modifyDate = modifyDate;
+		this.hit = hit;
+		this.deleted = deleted;
+		this.category = category;
+	}
+	
+	
+
+	public Board(int postNo, String nickName, String content, Date createDate, Date modifyDate, int hit, String deleted,
+			String category) {
+		super();
+		this.postNo = postNo;
+		this.nickName = nickName;
 		this.content = content;
 		this.createDate = createDate;
 		this.modifyDate = modifyDate;
@@ -42,9 +58,19 @@ public class Board {
 	public void setMemberNo(int memberNo) {
 		this.memberNo = memberNo;
 	}
+	
+	
 
 	public String getContent() {
 		return content;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
 	}
 
 	public void setContent(String content) {
@@ -93,10 +119,19 @@ public class Board {
 
 	@Override
 	public String toString() {
-		return "Board [postNo=" + postNo + ", memberNo=" + memberNo + ", content=" + content + ", createDate="
+		return "Board [postNo=" + postNo + ", nickName=" + nickName + ", content=" + content + ", createDate="
 				+ createDate + ", modifyDate=" + modifyDate + ", hit=" + hit + ", deleted=" + deleted + ", category="
 				+ category + "]";
 	}
+
+//	@Override
+//	public String toString() {
+//		return "Board [postNo=" + postNo + ", memberNo=" + memberNo + ", content=" + content + ", createDate="
+//				+ createDate + ", modifyDate=" + modifyDate + ", hit=" + hit + ", deleted=" + deleted + ", category="
+//				+ category + "]";
+//	}
+	
+	
 
 	
 	
